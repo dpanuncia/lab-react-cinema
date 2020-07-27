c0073ce9491c25b93bf74a4fa71763c86e30a4f4
Dionel E Panuncia <dpanuncia@gmail.com>
2020-07-27 14:05:10 -0400

    finished basic outline and alt 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-
+import {Route, Switch, Link } from "react-router-dom";
+import Movies from './components/Movies';
+import MovieDetails from './components/MovieDetails';
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Home />
+        <nav>
+          <Link to = "/Movies">Movies </Link>
+          <Link to = "/"> Home  </Link>
+          <Link to = "/Details">Movie Details  </Link>
+
+        </nav>
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route exact path="/Movies" render={(props) => <Movies {...props} />} />
+          <Route exact path="/Details" render={(props) => <MovieDetails {...props} />} />
+  
+        </Switch>
+    
       </div>
     );
   }
